ca87c596ba603156edda2ace34d4443c169084ff
zykowal <gzy520@qq.com>
2026-04-07 00:37:21 +0800

    feat: Add command history shortcut.

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -243,7 +243,9 @@ map("n", "<leader>lS", function()
   fzf.lsp_live_workspace_symbols()
 end, { desc = "Search workspace symbols" })
 
-map("n", "<leader>:", "q:", { desc = "Command history" })
+map("n", "<leader>:", function()
+  fzf.command_history()
+end, { desc = "Command history" })
 
 map("n", "<leader>,", function()
   fzf.live_grep_native()
