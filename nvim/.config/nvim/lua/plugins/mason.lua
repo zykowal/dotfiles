f72b6b7d4bdeddcc620907298a89af5633122865
zykowal <gzy520@qq.com>
2026-05-23 18:55:55 +0800

    feat: Add ts lsp support.

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -376,3 +376,33 @@ vim.lsp.config("gopls", {
     },
   },
 })
+
+vim.lsp.config("vtsls", {
+  settings = {
+    typescript = {
+      updateImportsOnFileMove = { enabled = "always" },
+      inlayHints = {
+        enumMemberValues = { enabled = true },
+        functionLikeReturnTypes = { enabled = true },
+        parameterNames = { enabled = "all" },
+        parameterTypes = { enabled = true },
+        propertyDeclarationTypes = { enabled = true },
+        variableTypes = { enabled = true },
+      },
+    },
+    javascript = {
+      updateImportsOnFileMove = { enabled = "always" },
+      inlayHints = {
+        enumMemberValues = { enabled = true },
+        functionLikeReturnTypes = { enabled = true },
+        parameterNames = { enabled = "literals" },
+        parameterTypes = { enabled = true },
+        propertyDeclarationTypes = { enabled = true },
+        variableTypes = { enabled = true },
+      },
+    },
+    vtsls = {
+      enableMoveToFileCodeAction = true,
+    },
+  },
+})
