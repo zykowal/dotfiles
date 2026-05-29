24329fa5eec713b9ec6197ee2957062a6b102915
zykowal <gzy520@qq.com>
2026-05-30 01:38:56 +0800

    feat: Using dsw Remove xxx<> tag.

--- nvim/.config/nvim/lua/plugins/surround.lua
+++ nvim/.config/nvim/lua/plugins/surround.lua
@@ -1,2 +1,11 @@
 require("nvim-surround").setup({
+  surrounds = {
+    w = {
+      add = function()
+        return nil
+      end,
+      find = "[%a_][%w_:]*%b<>",
+      delete = "^(.-<)().-(>)()$",
+    },
+  },
 })
